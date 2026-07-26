fd7e6480c0694744b6307df13be33977062b6f40
gaiidnabrb-pixel <gaiidnabrb@gmail.com>
2026-07-26 11:14:13 -0500

    Add custom ID and label to bug report button

--- src/commands/Core/help.js
+++ src/commands/Core/help.js
@@ -113,8 +113,8 @@ export async function createInitialHelpMenu(client) {
     embed.setTimestamp();
 
     const bugReportButton = new ButtonBuilder()
-        .setCustomId()
-        .setLabel("")
+        .setCustomId(BUG_REPORT_BUTTON_ID)
+        .setLabel("Report Bug")
         .setStyle(ButtonStyle.Danger);
 
     const supportButton = new ButtonBuilder()
